545e5008fdfc6a5cd719088570a127dbc1a9fa89
Jaime Pineda <jaime.e.pineda@gmail.com>
2015-11-30 17:56:10 +0100

    Update python script
    -puts back numpy, it is used to handle the noise level
    -keep lines to 80 col width (following PEP)

--- n2hp-test.py
+++ n2hp-test.py
@@ -1,8 +1,11 @@
 import pyspeckit
 from pyspeckit.spectrum.models import n2hp
+import numpy as np
 
 import astropy.units as u
-cube = pyspeckit.Spectrum('Core2_cent_N2Hp10.fits')
+
+rms=3.35E-2
+cube = pyspeckit.Spectrum('Core2_cent_N2Hp10.fits', error=np.ones(921)*rms)
 cube.xarr.refX = 93173.772e6*u.Hz
 cube.xarr.velocity_convention = 'radio'
 cube.xarr.convert_to_unit('km/s')
@@ -10,7 +13,9 @@ F=False
 T=True
 import matplotlib.pyplot as plt
 plt.ion()
-cube.Registry.add_fitter('n2hp_vtau', pyspeckit.models.n2hp.n2hp_vtau_fitter, 4)
-cube.specfit(fittype='n2hp_vtau', guesses=[3.94, 1.0, 0, 0.309], verbose_level=2, signal_cut=3, limitedmax=[F,T,T,T], limitedmin=[T,T,T,T], minpars=[0, 0, -1, 0.05], maxpars=[30.,50.,1,1.0], fixed=[F,F,F,F])
+cube.Registry.add_fitter('n2hp_vtau', pyspeckit.models.n2hp.n2hp_vtau_fitter,4)
+cube.specfit(fittype='n2hp_vtau', guesses=[3.94, 1.0, 0, 0.309], 
+    verbose_level=4, signal_cut=3, limitedmax=[F,T,T,T], limitedmin=[T,T,T,T], 
+    minpars=[0, 0, -1, 0.05], maxpars=[30.,50.,1,1.0], fixed=[F,F,F,F])
 cube.plotter(errstyle='fill')
 cube.specfit.plot_fit()
